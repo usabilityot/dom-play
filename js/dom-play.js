30df355d85f0c3cc2128d72b2aa9f71a41aa4849
usabilityot <usabilityot@hotmail.com>
2026-05-20 02:39:35 +0000

    adds changes

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,2 +1,15 @@
-alert("Hello from dom-play.js!");
-      
+/*
+
+
+   Create an app that on click actors lines are highlighted
+
+
+   With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+   Un-highlight (normal or otherwise) all lines from all other actors in the play
+  
+   The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+  
+   HTML "hooks" may be added to the page for proper operation
+
+
+*/
